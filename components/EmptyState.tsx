0d5b9cdad83fbebbef1c887924f0662b12d54af1
sina deyhim <sinadeyhim@sinas-MacBook-Pro.local>
2024-01-31 22:37:57 -0500

    polish

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const EmptyState: React.FC = () => {
+interface EmptyStateProps {
+  address?: string;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({ address }) => {
   return (
     <div className="loading-state bg-slate-900 h-100">
-      <div className="mb-4">No transactions found </div>
-      <div className="mr-4"> &#128546;</div>
+      {address ? (
+        <>
+          <div className="mb-4">No transactions found </div>
+          <div className="mr-4"> &#128546;</div>
+        </>
+      ) : (
+        <div className="mb-4">Please connect your wallet to continue </div>
+      )}
     </div>
   );
 };
--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -9,41 +9,54 @@ import { useAccount } from "wagmi";
 const TransactionList: React.FC = () => {
   const [transactions, setTransactions] =
     useState<AssetTransfersWithMetadataResponse>();
+  const [isLoading, setIsLoading] = useState(true);
   const [isEmpty, setIsEmpty] = useState(false);
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
 
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
+        setIsLoading(true);
+
         // Fetch transaction history for the address
         if (address) {
           const txHistory = await getTxs(address);
           setTransactions(txHistory);
-        }
 
-        if (!transactions?.transfers?.length) {
-          setIsEmpty(true);
+          // Check if there are no transactions
+          if (!txHistory?.transfers?.length) {
+            setIsEmpty(true);
+          } else {
+            setIsEmpty(false);
+          }
         }
       } catch (error) {
         console.error("Error fetching transactions:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchTransactions();
   }, [address]);
 
+  useEffect(() => {
+    // Update isEmpty state based on wallet connection status
+    setIsEmpty(!isConnected);
+  }, [isConnected]);
+
   return (
     <div className="bg-slate-slate-900 text-slate-200">
-      <h2 className="bg-slate-900 text-slate-200  p-4 text-2xl">
+      <h2 className="bg-slate-900 text-slate-200 p-4 text-2xl">
         Transaction List
       </h2>
       <ul>
-        {transactions?.transfers?.length ? (
-          <TxTable data={transactions.transfers} />
-        ) : isEmpty ? (
-          <EmptyState />
-        ) : (
+        {isLoading ? (
           <LoadingState />
+        ) : isEmpty ? (
+          <EmptyState address={address} />
+        ) : (
+          <TxTable data={transactions?.transfers || []} />
         )}
       </ul>
     </div>
